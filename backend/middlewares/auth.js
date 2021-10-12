a390e5c693e390a41047ff450968a0435af8d0de
Vitaliy <vitaliymontana@yandex.ru>
2021-10-13 02:26:13 +0300

    fixed evn

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,4 +1,4 @@
-const { NODE_ENV, JWT_SECRET } = process.env;
+const { JWT_SECRET = 'dev-secret' } = process.env;
 const jwt = require('jsonwebtoken');
 const UnauthorizedErr = require('../errors/unauthorized-err');
 const ForbiddenErr = require('../errors/forbidden-err');
@@ -10,7 +10,7 @@ module.exports = (req, res, next) => {
   }
   let payload;
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     throw new ForbiddenErr('Запрещено, нет прав доступа к содержимому!');
   }
